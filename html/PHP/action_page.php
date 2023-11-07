cb22408a840908b27436721d838b73c54ff2e8bc
HoangLong010 <dinhhoanglong010@gmail.com>
2023-11-07 23:30:56 +0700

    Thuc hanh dang ky thanh vien

--- html/PHP/action_page.php
+++ html/PHP/action_page.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-    Tổng:
-    <?php
-        $sum = $_GET['first'] + $_GET['last'];
-        echo $sum;
-    ?>
+
+<?php
+    echo'<div class="w3-container w3-center w3-animate-top">';
+    echo '<h1> Chào mừng '.$_GET['first'].' '. $_GET['last']. '</h1>';
+    echo'</div>'
+
+
+
+
+
+?>
+
     
 </body>
 </html>
--- html/PHP/index.php
+++ html/PHP/index.php
@@ -9,18 +9,18 @@
 
 <div class="w3-card-4">
   <div class="w3-container w3-brown">
-    <h2>Input Colors</h2>
+    <h2>Đăng ký thành viên</h2>
   </div>
   <form class="w3-container" action="action_page.php" method ='get'>
     <p>      
     <label class="w3-text-brown"><b>First Name</b></label>
-    <input class="w3-input w3-border w3-sand" name="first" type="number"></p>
+    <input class="w3-input w3-border w3-sand" name="first" type="text"></p>
     <p>      
     <label class="w3-text-brown"><b>Last Name</b></label>
-    <input class="w3-input w3-border w3-sand" name="last" type="number"></p>
+    <input class="w3-input w3-border w3-sand" name="last" type="text"></p>
     <p>
-    <button class="w3-btn w3-brown">Register</button></p>
-  </form>
+    <button class="w3-btn w3-brown">Đăng ký</button></p>
+  </form>`
 
   <?php
     // echo"MY FIRST PHP FILE";
