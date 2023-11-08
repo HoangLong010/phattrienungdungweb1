61e98046ffb02f0aeb78caf9ea20a8c2dea112cb
HoangLong010 <dinhhoanglong010@gmail.com>
2023-11-08 15:57:29 +0700

    Thuc hanh 3

---
+++ html/PHP3/thuchanh3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Đếm số lần kí tự xuất hiện:</h1>
+    <form action="thuchanh3s.php" method= 'get'>
+        <p>
+            <label>Nhập vào chuỗi kí tự: </label>
+            <input name='string' type="text">
+        </p>
+        <p>
+            <label>Kí tự cần tìm: </label>
+            <input  name='chucai' type="text">
+        </p>
+        <button>Tìm</button>
+    </form>
+</body>
+</html>
