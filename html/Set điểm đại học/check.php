1c62f27fabc29acddfff6590f55d3b4c27f8c260
HoangLong010 <dinhhoanglong010@gmail.com>
2023-11-22 15:06:20 +0700

    So sanh diem dai hoc

---
+++ html/Set điểm đại học/check.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $toan = $_GET['toan'];
+    $ly = $_GET['ly'];
+    $hoa = $_GET['hoa'];
+    $sum = $toan + $ly + $hoa;
+    $CNTT = 18;
+    $CNTTCLC = 22;
+    $KHDL = 18;
+    $DC = 17;
+    $MT = 15;
+
+    ?>
+
+    
+</body>
+</html>
---
+++ html/Set điểm đại học/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hãy nhập điểm của bạn</h1>
+    <form action="check.php" method= 'get'>
+        <p>
+            <label for="">Toán</label>
+            <input type="text" name='toan'>
+        </p>
+        <p>
+            <label for="">Lý</label>
+            <input type="text" name='ly'>
+        </p>
+        <p>
+            <label for="">Hoá</label>
+            <input type="text" name='hoa'>
+        </p>
+        <label for="">
+            CNTT:18
+            CNTT chất lượng cao: 22
+            KHDL: 18
+            Địa chất: 17
+            Môi trường: 15
+        </label><br>
+        <button>OK</button>
+        
+
+
+    </form>
+    
+</body>
+</html>
