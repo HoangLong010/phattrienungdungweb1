367b952299a6efec229a386e7ec0c8117233de84
HoangLong010 <dinhhoanglong010@gmail.com>
2023-11-28 18:39:05 +0700

    Slide 3

---
+++ html/28.11/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    foreach($items as $item){
+        echo"<img src = 'images/$item' width = 50px height = 50px alt ='$item'>";
+    }
+    ?>
+</body>
+</html>
