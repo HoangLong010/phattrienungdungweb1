4d9393d2b5f4d07c112391d184648635a0147694
HoangLong010 <dinhhoanglong010@gmail.com>
2023-11-28 18:51:50 +0700

    Slide 20

--- html/28.11/index.php
+++ html/28.11/index.php
@@ -26,5 +26,20 @@
         }
         
     ?>
+    <?php
+    $items = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
+    foreach($items as $item){
+        echo"<img src = 'images/$item' width = 50px height = 50px alt ='$item'>";
+    }
+    ?>
+    <h1>function</h1>
+    <?php
+    $items = array('item5.png', 'item6.png');
+    foreach($items as $item){
+        echo"<img src = 'images/$item' width = 50px height = 50px alt ='$item'>";
+    }
+    ?>
+    <h1>Dinh Hoang Long 2121050394</h1>
+
 </body>
 </html>
